87c5da5a37e42e6c1984d231ec631a37ce01fb72
Spamty Server <server@spamty.eu>
2016-04-17 07:14:31 -0400

    Works with bitbucket

--- spamty.cdn.sh
+++ spamty.cdn.sh
@@ -4,10 +4,11 @@
 
 # Die folgende Variable speichert den Pfad zum Repository um das es geht.
 #GIT_REPO=https://spamtybot:uDgmVl57UnWtLNl@git.spamty.eu/spamty/cdn.git
-GIT_REPO=/home/philr/gogs-repositories/spamty/cdn.git
+#GIT_REPO=/home/philr/gogs-repositories/spamty/cdn.git
+GIT_REPO=https://spamty-bot:AzzBqm49ms3NP9BB8@bitbucket.org/spamty/cdn.git
 
 # Die folgende Variable speichert den Pfad zum Webroot
-PUBLIC_WWW=/var/www/virtual/philr/cdn.spamty.eu
+PUBLIC_WWW=/var/www/cdn.spamty.eu
 
 # Webroot löschen
 rm -rf $PUBLIC_WWW
