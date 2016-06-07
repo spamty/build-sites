2f6a8fa0180c0c7db8074d5834e1be023eb7f62a
philipp-r <philipp-r@server.spamty.eu>
2016-06-07 15:53:54 -0400

    update

--- webhook.php
+++ webhook.php
@@ -1,6 +1,32 @@
 <?php
 
-print_r($_SERVER);
+echo "test";
 
-$shellStatus = shell_exec("/var/build-sites/spamty.website.sh");
-print_r($shellStatus);
+print_r("HTTP_X_EVENT_KEY ".$_SERVER['HTTP_X_EVENT_KEY']."   ");
+print_r("HTTP_X_REQUEST_UUID ".$_SERVER['HTTP_X_REQUEST_UUID']."   ");
+print_r("HTTP_X_ATTEMPT_NUMBER ".$_SERVER['HTTP_X_ATTEMPT_NUMBER']."   ");
+print_r("HTTP_X_HOOK_UUID ".$_SERVER['HTTP_X_HOOK_UUID']."   ");
+
+if( $_GET['site'] == "api-doc" ){
+	$shReturned = shell_exec("/var/build-sites/spamty.api-doc.sh");
+	print_r($shReturned);
+	die("Done.");
+}elseif( $_GET['site'] == "api" ){
+	$shReturned = shell_exec("/var/build-sites/spamty.api.sh");
+	print_r($shReturned);
+	die("Done.");
+}elseif( $_GET['site'] == "blog" ){
+	$shReturned = shell_exec("/var/build-sites/spamty.blog.sh");
+	print_r($shReturned);
+	die("Done.");
+}elseif( $_GET['site'] == "cdn" ){
+	$shReturned = shell_exec("/var/build-sites/spamty.cdn.sh");
+	print_r($shReturned);
+	die("Done.");
+}elseif( $_GET['site'] == "website" ){
+	$shReturned = shell_exec("/var/build-sites/spamty.website.sh");
+	print_r($shReturned);
+	die("Done.");
+}else{
+	die("Invalid value for GET *site*.");
+}
