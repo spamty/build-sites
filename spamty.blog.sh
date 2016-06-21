5a062c9d552024edb0af94c5f1a3d2af257e3c58
philipp-r <philipp-r@users.noreply.github.com>
2016-06-21 15:45:51 +0200

    Clone from GitHub Pages

--- spamty.blog.sh
+++ spamty.blog.sh
@@ -1,38 +1,22 @@
 #!/bin/sh
-# Source: http://oldarticles.kahlil.co/2011/07/24/uberkyll/
-# https://gist.github.com/philipp-r/a07009762dea99929784
 
-# Die folgende Variable speichert den Pfad zum Repository um das es geht.
-#GIT_REPO=https://spamtybot:uDgmVl57UnWtLNl@git.spamty.eu/spamty/blog.git
-GIT_REPO=/var/git/blog.git
+# GitHub repository to clone
+GIT_REPO=https://github.com/Spamty/blog.git
+# Clone destination
+GIT_CLONE_DEST=/var/git/blog.git
 
-# Die folgende Variable speichert den Pfad zum tmp Ordner in dem dann der Jekyll
-# Befehl ausgeführt wird um die deine Seite in den Webroot zu befördern.
-TMP_GIT_CLONE=/tmp/build_spamtyblog
+# Remove destination
+printf "Deleting $GIT_CLONE_DEST \n"
+rm -rf $GIT_CLONE_DEST
 
-# Die folgende Variable speichert den Pfad zum Webroot
-PUBLIC_WWW=/var/www/blog.spamty.eu
-
-# Mit "git clone" wird Dein Repository in das tmp-Verzeichnis geklont
-git clone $GIT_REPO $TMP_GIT_CLONE
-printf "Cloning files with git in /tmp \n"
-
-# Jekyll generiert die Seite aus dem tmp-Verzeichnis heraus
-# in den Webroot hinein.
-jekyll build --source $TMP_GIT_CLONE --destination $PUBLIC_WWW
-printf "Jekyll build \n"
-
-# Das tmp-Verzeichnis wird gelöscht
-rm -Rf $TMP_GIT_CLONE
-printf "Deleting /tmp \n"
+# Clone
+printf "Cloning files with git in $GIT_CLONE_DEST \n"
+git clone --bare $GIT_REPO $GIT_CLONE_DEST
 
 # File permissions for new files
-printf "Set new file permissions \n"
-# owner is git
-# Group is web-dev
-chgrp -R web-dev $PUBLIC_WWW
-# Permissions rwx rwx r-x
-chmod -R 775 $PUBLIC_WWW
+printf "Set new owner and group \n"
+# owner & group is git
+chown git:git $GIT_CLONE_DEST -R
 
 # Das Shell-Programm wird beendet
 printf "Done \n"
