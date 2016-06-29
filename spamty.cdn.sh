2a149fff4d952eed5c316522e23698f0790edc87
philipp-r <philipp-r@server.spamty.eu>
2016-06-29 10:44:13 -0400

    AWS

--- spamty.cdn.sh
+++ spamty.cdn.sh
@@ -12,6 +12,9 @@ PUBLIC_WWW=/var/www/cdn.spamty.eu
 # deploy website
 ./deploy-web.sh $GIT_REPO $PUBLIC_WWW
 
+# Sync with AWS S3
+aws s3 sync $PUBLIC_WWW s3://spamty-cdn --delete --acl public-read
+
 # fetch in lokal git repo
 ./git-fetch.sh $GIT_REPO $GIT_REPO_LOKAL
 
