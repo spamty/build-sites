1f3ad8b5f5ae18fa9b256137a25edba5d5c0f69f
philipp-r <philipp-r@users.noreply.github.com>
2016-07-26 20:40:54 +0200

    update

--- spamty.blog.sh
+++ spamty.blog.sh
@@ -5,17 +5,6 @@ GIT_REPO=git@github.com:philipp-r/spamty-blog.git
 # Lokal git repo
 GIT_REPO_LOKAL=/var/git/blog.git
 
-# path to webroot
-# PUBLIC_WWW=/var/www/blog.spamty.eu
-
-# branch to deploy
-# NOT required because hosted with GitHub Pages
-# DEPLOY_BRANCH=master
-
-
-# deploy website
-# NOT required because hosted with GitHub Pages
-# ./deploy-web.sh $GIT_REPO $PUBLIC_WWW $DEPLOY_BRANCH
 
 # fetch in lokal git repo
 ./git-fetch.sh $GIT_REPO $GIT_REPO_LOKAL gh-pages:gh-pages
