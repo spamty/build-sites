9ab40cb9fd2c820771fd3e5dd4c57a4c16b7eb80
Spamty Server <spamty@vps.server.com>
2016-08-04 11:41:21 -0400

    update permissions

--- spamty.bumpy-booby.sh
+++ spamty.bumpy-booby.sh
@@ -19,11 +19,6 @@ mkdir $TMP_GIT_CLONE
 # Backup /var/www/bugtracker/database
 printf "Backup database \n"
 cp -R $PUBLIC_WWW/database $TMP_GIT_CLONE/database
-# Backup /var/www/bugtracker/.htaccess and .htpasswd
-printf "Backup .htaccess, .htpasswd \n"
-cp $PUBLIC_WWW/.htaccess $TMP_GIT_CLONE/.htaccess
-cp $PUBLIC_WWW/.htpasswd $TMP_GIT_CLONE/.htpasswd
-
 
 # deploy website
 ./deploy-web.sh $GIT_REPO $PUBLIC_WWW $DEPLOY_BRANCH
@@ -31,25 +26,20 @@ cp $PUBLIC_WWW/.htpasswd $TMP_GIT_CLONE/.htpasswd
 # Restore /var/www/bugtracker/database
 printf "Restore database \n"
 cp -R $TMP_GIT_CLONE/database $PUBLIC_WWW/database
-# Restore /var/www/bugtracker/.htaccess and .htpasswd
-printf "Restore .htaccess, .htpasswd \n"
-cp $TMP_GIT_CLONE/.htaccess $PUBLIC_WWW/.htaccess
-cp $TMP_GIT_CLONE/.htpasswd $PUBLIC_WWW/.htpasswd
 
 # database has to be readable & writable for apache
 # owner is spamty, Group is www-data (apache)
 # Permissions rwx rwx ---
 printf "Permissions for database directory \n"
+chgrp www-data -R $PUBLIC_WWW/database
 chmod 770 -R $PUBLIC_WWW/database
 
 # Das tmp-Verzeichnis wird gelöscht
-#rm -Rf $TMP_GIT_CLONE
-#printf "Deleting /tmp \n"
-
+rm -Rf $TMP_GIT_CLONE
+printf "Deleting /tmp \n"
 
 # fetch in lokal git repo
 ./git-fetch.sh $GIT_REPO $GIT_REPO_LOKAL
-
 
 # Das Shell-Programm wird beendet
 printf "End \n"
