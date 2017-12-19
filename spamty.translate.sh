1d9f02b77e069f54d548fcb35844d4e6e08b78c7
Spamty VPS <spamty@vpn.example.com>
2017-12-19 14:23:11 +0000

    translate

---
+++ spamty.translate.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# GitHub repository
+GIT_REPO=git@github.com:Spamty/translate.git
+# Lokal git repo
+GIT_REPO_LOKAL=/home/spamty/git/translate.git
+
+# path to webroot
+PUBLIC_WWW=/home/spamty/www/spamty.eu/translate
+
+# branch to deploy
+DEPLOY_BRANCH=master
+
+# deploy website
+./deploy-web.sh $GIT_REPO $PUBLIC_WWW $DEPLOY_BRANCH
+
+# fetch in lokal git repo
+./git-fetch.sh $GIT_REPO $GIT_REPO_LOKAL
+
+# Das Shell-Programm wird beendet
+printf "End \n"
+exit
