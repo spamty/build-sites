4b324a13e0cda0dc80649e64a71b886676645203
Philipp R <bitbucket@8qq.de>
2018-06-26 20:15:52 +0000

    fetch done with bitbucket pipelines now

--- spamty.website.sh
+++ spamty.website.sh
@@ -5,10 +5,6 @@ GIT_REPO=git@bitbucket.org:spamty/website.git
 # Lokal git repo
 GIT_REPO_LOKAL=/home/spamty/git/website.git
 
-# GitHub wiki
-GIT_REPO_WIKI=git@bitbucket.org:spamty/website.git/wiki
-# Lokal git repo for wiki
-GIT_REPO_WIKI_LOKAL=/home/spamty/git/website.wiki.git
 
 # path to webroot
 PUBLIC_WWW=/home/spamty/www/spamty.eu
@@ -22,11 +18,7 @@ DEPLOY_BRANCH=master
 # deploy translations (they are deleted from webroot)
 ./spamty.translate.sh
 
-# fetch in lokal git repo
-./git-fetch.sh $GIT_REPO $GIT_REPO_LOKAL
 
-# fetch wiki
-./git-fetch.sh $GIT_REPO_WIKI $GIT_REPO_WIKI_LOKAL
 
 # Das Shell-Programm wird beendet
 printf "End \n"
